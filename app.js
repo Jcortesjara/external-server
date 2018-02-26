42a97a6ba67cfb9924f37aa1150ba0be84a5d0ab
jose.cortes <jose.cortes@pragma.com.co>
2018-02-26 14:04:25 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+const basicAuth = require('express-basic-auth');
+const bodyParser    = require('body-parser');
+
+
+app.use(bodyParser.json());
+
+app.use(basicAuth({
+    users: { 'jose': 'rube' }
+}));
+
+app.post('/notifyOrder', function (req, res) {
+    console.log(req.body);
+    res.send('funcionando ando!');
+    console.log("llego el post");
+});
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+    console.log("llego el get");
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
