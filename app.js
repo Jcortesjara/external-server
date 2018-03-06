3aeebf5ffc558b5b11b14faa64aecdc7ecb0d281
Rubelio Cardona Giraldo <rubelio.cardona@PRAGMA.COM.CO>
2018-03-06 09:14:20 -0500

    test

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 const basicAuth = require('express-basic-auth');
 const bodyParser    = require('body-parser');
+const qt = 2;
 
 
 app.use(bodyParser.json());
@@ -19,6 +20,106 @@ app.post('/notifyOrder', function (req, res) {
 app.get('/', function (req, res) {
     res.send('Hello World!');
     console.log("llego el get");
+});
+
+app.get('/getStockUnit', function (req, res) {
+    console.log('GET getStockUnit request sku: ' + req.query.sku);
+    var respond = {
+        id: Math.floor(Math.random() * Math.floor(100)),
+        sku : req.query.sku,
+        quantity : qt
+    }
+    res.send(respond);
+    console.log('GET external-inventory respond: ' + JSON.stringify(respond));
+});
+
+app.get('/getStockUnits', function (req, res) {
+    console.log('GET getStockUnits request id: ' + req.query.id);
+    var respond = {
+        id : req.query.id,
+        stockUnits : null
+    }
+    switch (req.query.id) {
+        case "5a96dad55dc69532587d3956":
+            var data = [
+                {
+                    id: Math.floor(Math.random() * Math.floor(100)),
+                    sku : "12",
+                    quantity : qt
+                },
+                {
+                    id: Math.floor(Math.random() * Math.floor(100)),
+                    sku : "13",
+                    quantity : qt
+                },
+                {
+                    id: Math.floor(Math.random() * Math.floor(100)),
+                    sku : "14",
+                    quantity : qt
+                },
+                {
+                    id: Math.floor(Math.random() * Math.floor(100)),
+                    sku : "15",
+                    quantity : qt
+                },
+                {
+                    id: Math.floor(Math.random() * Math.floor(100)),
+                    sku : "16",
+                    quantity : qt
+                },
+                {
+                    id: Math.floor(Math.random() * Math.floor(100)),
+                    sku : "17",
+                    quantity : qt
+                },
+                {
+                    id: Math.floor(Math.random() * Math.floor(100)),
+                    sku : "18",
+                    quantity : qt
+                },
+                {
+                    id: Math.floor(Math.random() * Math.floor(100)),
+                    sku : "19",
+                    quantity : qt
+                }
+            ];
+            respond.stockUnits = data;
+            break;
+        case "5a9418558f48c02178bbb6qt":
+            var data = [
+                {
+                    id: Math.floor(Math.random() * Math.floor(100)),
+                    sku : "123abc",
+                    quantity : qt
+                },
+                {
+                    id: Math.floor(Math.random() * Math.floor(100)),
+                    sku : "a1b2c3",
+                    quantity : qt
+                }
+            ];
+            respond.stockUnits = data;
+            break;
+        case "5a9d5ef05dc69544980be831":
+            var data = [
+                {
+                    id: Math.floor(Math.random() * Math.floor(100)),
+                    sku : "qazwsx",
+                    quantity : qt
+                },
+                {
+                    id: Math.floor(Math.random() * Math.floor(100)),
+                    sku : "wsxqaz",
+                    quantity : qt
+                }
+            ];
+            respond.stockUnits = data;
+            break;
+        default:
+            break;
+    }
+    res.send(respond);
+    console.log('GET external-inventory respond: ' + JSON.stringify(respond));
 });
 
 app.listen(3000, function () {
